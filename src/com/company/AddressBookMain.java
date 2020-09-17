cfe2610cf68e4c64d2e9d488c40ba8780956809d
mukesh <mukeshmahajan501@gmail.com>
2020-09-18 03:56:26 +0530

    [Mukesh]Added:new person to address book

--- src/com/company/AddressBookMain.java
+++ src/com/company/AddressBookMain.java
@@ -1,8 +1,46 @@
 package com.company;
-
+import java.util.LinkedList;
+import java.util.Scanner;
 public class AddressBookMain {
 
     public static void main(String[] args) {
-        System.out.println("welcome to address book Application!!! ");
+        LinkedList<Person> addressBookMain = new LinkedList<>();
+        AddressBook addressBook = new AddressBook();
+        Person person;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Address Book !");
+        int temp = 0;
+        while (temp == 0) {
+            System.out.print("\n1. Add Person." +
+                    "\n2. View Address Book." +
+                    "\n3. Exit." +
+                    "\n Enter your choice: ");
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    person = addressBook.addPerson();
+                    addressBookMain.add(person);
+                    break;
+
+                case 2:
+                    if (addressBookMain.isEmpty()) {
+                        System.out.println("Address Book is Empty.");
+                    } else {
+                        for (Person details : addressBookMain) {
+                            System.out.println(details.toString());
+                        }
+                    }
+                    break;
+
+                case 3:
+                    temp = 1;
+                    System.out.println("Exit.");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice.");
+                    break;
+            }
+        }
     }
 }
