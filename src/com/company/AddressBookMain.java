6e9a73a46c985b9be43e58c7c439dbc795d7e2cc
mukesh <mukeshmahajan501@gmail.com>
2020-09-18 04:57:22 +0530

    [Mukesh]Added:delete person using person name

--- src/com/company/AddressBookMain.java
+++ src/com/company/AddressBookMain.java
@@ -5,42 +5,52 @@ import java.util.Scanner;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        LinkedList<Person> addressBookMain = new LinkedList<>();
+        LinkedList<Person> list = new LinkedList<>();
         AddressBook addressBook = new AddressBook();
         Person person;
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Address Book !");
-        boolean flag = false;
+        boolean flag=false;
         while (flag==false) {
             System.out.print("\n1. Add Person." +
                     "\n2. View Address Book." +
                     "\n3. Edit details." +
-                    "\n4. Exit." +
+                    "\n4. Delete person." +
+                    "\n5. Exit." +
                     "\n Enter your choice: ");
             int choice = input.nextInt();
             switch (choice) {
                 case 1:
                     person = addressBook.addPerson();
-                    addressBookMain.add(person);
+                    list.add(person);
                     break;
 
                 case 2:
-                    if (addressBookMain.isEmpty()) {
+                    if (list.isEmpty()) {
                         System.out.println("Address Book is Empty.");
                     } else {
-                        for (Person details : addressBookMain) {
+                        for (Person details : list) {
                             System.out.println(details.toString());
                         }
                     }
                     break;
+
                 case 3:
-                    if (addressBookMain.isEmpty()) {
+                    if (list.isEmpty()) {
                         System.out.println("Address Book Empty.");
                     } else {
-                        addressBookMain = addressBook.editPerson(addressBookMain);
+                        list = addressBook.editPerson(list);
                     }
                     break;
+
                 case 4:
+                    if (list.isEmpty()) {
+                        System.out.println("Address Book Empty.");
+                    } else {
+                        list = addressBook.deletePerson(list);
+                    }
+                    break;
+                case 5:
                     flag=true;
                     System.out.println("Exit.");
                     break;
