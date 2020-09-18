550372ecbd8a904c10fa9e871cf00c32e2c902b5
mukesh <mukeshmahajan501@gmail.com>
2020-09-18 17:26:58 +0530

    [Mukesh]Added:sort the entries in the address book by city,state or zip

--- src/com/company/AddressBookMain.java
+++ src/com/company/AddressBookMain.java
@@ -10,22 +10,17 @@ public class AddressBookMain {
         Person person;
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Address Book !");
-        int temp = 0;
-        while (temp == 0) {
-            System.out.print("\n1. Add Person." +
-                    "\n2. View Address Book." +
-                    "\n3. Edit details." +
-                    "\n4. Delete person." +
-                    "\n5. Sort by name." +
-                    "\n6. Exit." +
-                    "\n Enter your choice: ");
+        boolean flag = false;
+        while (flag == false) {
+            System.out.print("\n1. Add Person." + "\n2. View Address Book." + "\n3. Edit details."
+                    + "\n4. Delete person." + "\n5. Sort by name." + "\n6. Sort by city." + "\n7. Sort by state."
+                    + "\n8. Sort by Zip." + "\n9. Exit." + "\n Enter your choice: ");
             int choice = input.nextInt();
             switch (choice) {
                 case 1:
                     person = addressBook.addPerson(list);
                     list.add(person);
                     break;
-
                 case 2:
                     if (list.isEmpty()) {
                         System.out.println("Address Book is Empty.");
@@ -43,7 +38,6 @@ public class AddressBookMain {
                         list = addressBook.editPerson(list);
                     }
                     break;
-
                 case 4:
                     if (list.isEmpty()) {
                         System.out.println("Address Book Empty.");
@@ -51,7 +45,6 @@ public class AddressBookMain {
                         list = addressBook.deletePerson(list);
                     }
                     break;
-
                 case 5:
                     if (list.isEmpty()) {
                         System.out.println("Address Book Empty.");
@@ -59,12 +52,31 @@ public class AddressBookMain {
                         addressBook.sortByName(list);
                     }
                     break;
-
                 case 6:
-                    temp = 1;
+                    if (list.isEmpty()) {
+                        System.out.println("Address Book Empty.");
+                    } else {
+                        addressBook.sortByCity(list);
+                    }
+                    break;
+                case 7:
+                    if (list.isEmpty()) {
+                        System.out.println("Address Book Empty.");
+                    } else {
+                        addressBook.sortByState(list);
+                    }
+                    break;
+                case 8:
+                    if (list.isEmpty()) {
+                        System.out.println("Address Book Empty.");
+                    } else {
+                        addressBook.sortByZip(list);
+                    }
+                    break;
+                case 9:
+                    flag = true;
                     System.out.println("Exit.");
                     break;
-
                 default:
                     System.out.println("Invalid Choice.");
                     break;
